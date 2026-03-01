1f1ea5ee2bad7a994a005acd8f3cfdaa149f606f
pmozola <mozola.pawel@gmail.com>
2026-03-01 20:39:51 +0100

    envs

--- src/frontend/garmin-tools/src/app/app.config.ts
+++ src/frontend/garmin-tools/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { API_URL } from './core/api-token';
-import { environment } from './environment/environment';
+import { environment } from '../environments/environment';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { baseUrlInterceptor } from './interceptors/base-url-interceptor';
 import { loadingInterceptor } from './interceptors/loading-interceptor';
